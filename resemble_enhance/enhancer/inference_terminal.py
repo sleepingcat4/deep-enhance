67d96a4f120e2d846f669ea601e2afd5dbef4bd4
TAWSIF AHMED <stanfordblaze4@gmail.com>
2025-01-21 00:19:59 +0600

    added run_dir

--- resemble_enhance/enhancer/inference_terminal.py
+++ resemble_enhance/enhancer/inference_terminal.py
@@ -5,14 +5,14 @@ from resemble_enhance.enhancer.inference import enhance
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def enhance_audio(input_audio_path, output_audio_path, solver="midpoint", nfe=64, tau=0.5):
+def enhance_audio(input_audio_path, output_audio_path, run_dir, solver="midpoint", nfe=64, tau=0.5):
     dwav, sr = torchaudio.load(input_audio_path)
     dwav = dwav.mean(dim=0)
-    enhanced_audio, new_sr = enhance(dwav, sr, device, nfe=nfe, solver=solver, lambd=0.1, tau=tau)
+    enhanced_audio, new_sr = enhance(dwav, sr, device, nfe=nfe, solver=solver, lambd=0.1, tau=tau, run_dir=run_dir)
     torchaudio.save(output_audio_path, enhanced_audio.unsqueeze(0), new_sr)
     print(f"Enhanced audio saved to: {output_audio_path}")
     
-def enhance_folder(input_folder, output_folder, solver="midpoint", nfe=64, tau=0.5):
+def enhance_folder(input_folder, output_folder, run_dir, solver="midpoint", nfe=64, tau=0.5):
     input_folder = Path(input_folder)
     output_folder = Path(output_folder)
 
@@ -26,4 +26,4 @@ def enhance_folder(input_folder, output_folder, solver="midpoint", nfe=64, tau=0
 
     for input_file in input_files:
         output_audio = output_folder / f"{input_file.stem}_enhanced.wav"
-        enhance_audio(input_file, output_audio, solver, nfe, tau)
+        enhance_audio(input_file, output_audio, run_dir, solver, nfe, tau)
